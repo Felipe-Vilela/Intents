b0cda344c685660507b6f2f3dd891aa3bb8b386a
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-12 22:24:46 -0300

    cleanup: removing dead code

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
@@ -2,9 +2,6 @@ package br.edu.ifsp.scl.ads.prdm.sc303898x.intents
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
