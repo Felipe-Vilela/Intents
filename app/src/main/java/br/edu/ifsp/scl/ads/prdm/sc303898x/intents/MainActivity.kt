5a94acadd6c1da66ec7108b13cb594144dd39b0e
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-12 22:23:21 -0300

    feat: not obsolete way

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
@@ -2,6 +2,11 @@ package br.edu.ifsp.scl.ads.prdm.sc303898x.intents
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultCaller
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.intents.databinding.ActivityMainBinding
 
@@ -9,7 +14,9 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    
+
+    private lateinit var narl: ActivityResultLauncher<Intent>
+
     companion object Constants{
         const val NEW_ACTIVITY_REQUEST_CODE = 0
         const val EXTRA_PARAMETER = "EXTRA_PARAMETER"
@@ -19,6 +26,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
 
+        narl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {resultado ->
+            if ( resultado.resultCode == RESULT_OK) {
+                resultado.data?.getStringExtra(EXTRA_PARAMETER).let { parametro ->
+                    amb.parametroTv.text = parametro
+                }
+            }
+        }
+
         amb.toolbarIn.toolbar.apply {
             subtitle = localClassName
             setSupportActionBar(this)
@@ -27,16 +44,7 @@ class MainActivity : AppCompatActivity() {
         amb.entrarParametroBt.setOnClickListener{
             Intent(this@MainActivity, NewActivity::class.java).apply {
                 putExtra(EXTRA_PARAMETER, amb.parametroTv.text.toString())
-                startActivityForResult(this@apply, NEW_ACTIVITY_REQUEST_CODE)
-            }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == NEW_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
-            data?.getStringExtra(EXTRA_PARAMETER).let { parametro ->
-                amb.parametroTv.text = parametro
+                narl.launch(this)
             }
         }
     }
