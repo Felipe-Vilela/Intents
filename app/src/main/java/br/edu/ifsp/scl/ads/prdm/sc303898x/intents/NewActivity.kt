55b5bf62cf9366ab9204b782f5f130f9deca882a
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-12 21:51:35 -0300

    feat: java format

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/NewActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/NewActivity.kt
@@ -3,6 +3,7 @@ package br.edu.ifsp.scl.ads.prdm.sc303898x.intents
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.ads.prdm.sc303898x.intents.MainActivity.Constants.EXTRA_PARAMETER
 import br.edu.ifsp.scl.ads.prdm.sc303898x.intents.databinding.ActivityNewBinding
 
 class NewActivity : AppCompatActivity() {
@@ -21,11 +22,11 @@ class NewActivity : AppCompatActivity() {
         }
 
         anb.enviarParametroBt.setOnClickListener{
-            anb.parametroEt.text.toString().let { parametro ->
-                setResult(RESULT_OK, Intent().apply {
-                    this.putExtra(MainActivity.EXTRA_PARAMETER, parametro)
-                })
-            }
+            val parametro = anb.parametroEt.text.toString()
+            val intent = Intent()
+            intent.putExtra(EXTRA_PARAMETER, parametro)
+            setResult(RESULT_OK, intent)
+            finish()
         }
     }
 }
