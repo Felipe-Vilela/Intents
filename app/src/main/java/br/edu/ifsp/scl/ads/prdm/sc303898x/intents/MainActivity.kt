0212fdb332aec8ef01a5dad800b5ab340ca883d5
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-26 19:50:37 -0300

    feat: 2 jeitos

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc303898x/intents/MainActivity.kt
@@ -1,11 +1,16 @@
 package br.edu.ifsp.scl.ads.prdm.sc303898x.intents
 
 import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc303898x.intents.databinding.ActivityMainBinding
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
@@ -15,7 +20,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var narl: ActivityResultLauncher<Intent>
 
     companion object Constants{
-        const val NEW_ACTIVITY_REQUEST_CODE = 0
         const val EXTRA_PARAMETER = "EXTRA_PARAMETER"
     }
     
@@ -45,4 +49,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.view_mi -> {
+                val url = amb.parametroTv.text.toString().toUri()
+                val navegadorIntent = Intent(ACTION_VIEW, url)
+                startActivity(navegadorIntent)
+
+                amb.parametroTv.text.toString().let{urlTexto ->
+                    Uri.parse(urlTexto).let { url->
+                        Intent(ACTION_VIEW, url).apply {
+                            startActivity(this)
+                        }
+                    }
+                }
+
+                true }
+            R.id.call_mi -> { true }
+            R.id.dial_mi -> { true }
+            R.id.pick_mi -> { true }
+            R.id.chosser_mi -> { true }
+            else -> { false }
+        }
+    }
+
 }
